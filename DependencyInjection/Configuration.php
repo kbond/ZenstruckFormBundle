67240e9048b734f1b5846faf14c341d56dc55de4
Kevin Bond <kevinbond@gmail.com>
2019-11-27 16:10:42 -0500

    add BC layer for Configuration::getConfigTreeBuilder()

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -12,8 +12,14 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('zenstruck_form');
+        $treeBuilder = new TreeBuilder('zenstruck_form');
+
+        // Keep compatibility with symfony/config < 4.2
+        if (\method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $rootNode = $treeBuilder->root('zenstruck_form');
+        }
 
         $rootNode
             ->children()
